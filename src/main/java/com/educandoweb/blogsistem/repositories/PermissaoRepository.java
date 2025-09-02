6dc7a716aff34d0fd2e67ad7a13f981a36edb03b
Fuelwild796 <165859108+Fuelwild796@users.noreply.github.com>
2025-09-02 12:50:58 -0300

    PermissaoRepository

---
+++ src/main/java/com/educandoweb/blogsistem/repositories/PermissaoRepository.java
@@ -0,0 +1,7 @@
+package com.educandoweb.blogsistem.repositories;
+
+import com.educandoweb.blogsistem.entities.Permissao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+}
